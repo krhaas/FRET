bb0af000944d3523d2856eb3f7ae1034cb9e91a6
Kevin Haas <krhaas@users.noreply.github.com>
2014-12-23 00:42:53 -0800

    removed unused lines in getPhi

--- EM/getPhi.m
+++ EM/getPhi.m
@@ -9,11 +9,8 @@ Dnx=Chebs{11};
 py=zeros(length(Fdata),1);
 needgrad=nargout;
 
-%D=exp(Dnx*CD);
 D=Dnx*CD;
 
-%iterate through each photon trajectory
-    
 Nv=64;%number of eignevectors of hamiltonian to compute and iterate.  increase for accuracy
 
 %Tad=Fdata.Tad;%both acceptor and donor arrival times
@@ -36,7 +33,6 @@ N=length(xref);
 p0=p0/sum(p0.*w0);
 p02=p0;
 p02=max(p0,1e-10);  %1e-10 Threshold so you only need 64 eigenvectors to get derivative right
-%p02=max(p0,1e-20);
 p02=p02/sum(p02.*w0);
 
 
@@ -57,7 +53,6 @@ clear opts
 opts.issym=1;
 opts.v0=ones(size(Se,1),1);
 opts.disp=0;
-%opts.tol=eps/1e115;
 try
     [Q,LQ,flag]=eigs(Ke,Se,Nv,'sm',opts);%generalized eigenvalue probelm, give smallest vec/vals
     assert(sum(flag) == 0,'nonconverged eigenvalues')
@@ -135,10 +130,6 @@ btemp(1)=1;
 btemp=Qdark'*btemp;
 
 %Prior probability Initialization 
-
-%piAD = sqrt(piA.*piD);
-%btemp = Qxdark'*piAD;
-atemp=btemp;
 %Apply initial wait time to start and end of trajectory
 
 
@@ -149,7 +140,6 @@ atemp=btemp;
 Anorm(Times+2)=sum(btemp.*atemp);%make sure integral norms to 1
 atemp=atemp/Anorm(end);
 btemp=btemp/Anorm(Times+2)/Anorm(Times+1);
-%betax(:,end)=Qx*btemp;
 
 %% Calculate Likelihood 
 py=-sum(log(Anorm)+pyNorm/(Times+2));
@@ -178,8 +168,7 @@ gradD=sum(w0.*p0.*Pdxdx);
 Ginit=Qdark*(btemp+atemp)./LQ;
 Ginit(1)=0;
 Ginit=[1 ;zeros(Nv-1,1)]*Ginit';
-%GHo = GHo + Ginit;
-%}
+
 
 %Calculate relative time averaged position 
 Phi = sum(Qx2.*(Qx2*GHo),2);
@@ -191,122 +180,3 @@ gradF=-D.*p0.*dPhi/2;
 
 end
 end
-
-
-%{
-% Old Alpha
-Anorm=ones(1,Times+2);
-Anorm(1)=norm(atemp);
-%atemp=atemp/Anorm(1);
-alpha=zeros(Nv,Times+1);
-alpha(:,1)=atemp;
-
-%alphax(:,1)=Qx*atemp;
-
-%iterate through trajectory for alphas
-for i=1:Times
-    %DTs=Tad(i)-Tad(i-1);%time between photons
-    
-    if( iTad(i) ) %apply donor or acceptor operator
-        atemp = PD*(atemp.*exp(-LQdark*DTs(i)));
-    else
-        atemp = PA*(atemp.*exp(-LQdark*DTs(i)));       
-    end
-    
-    Anorm(i+1)=norm(atemp);
-    atemp=atemp/Anorm(i+1);
-    alpha(:,i+1)=atemp;
-   
-    %alphax(:,i)=Qx*atemp;
-end
-%}
-
-%{ 
-    %Old Beta Recursion
-    G=zeros(Nv,Nv);%gradient expectation
-someones=ones(1,Nv);
-for i=Times:-1:1
-    %DT=Tad(i+1)-Tad(i);
-    DT=DTs(i);
-    expLQDT=exp(-LQdark*DT);
-    
-    if( iTad(i) )
-        btemp = PD*btemp;
-    else
-        btemp = PA*btemp;
-    end
-    
-    %time smooth the derivative of operator
-    eLQDT=(expLQDT*someones);
-    outer=(alpha(:,i)*btemp');
-    G=G+outer.*(diag(expLQDT*DT)+iLQdark.*(eLQDT-eLQDT'));
-    
-    btemp=btemp.*expLQDT;
-   
-    %beta(:,i)=btemp;
-    %betax(:,i)=Qx*btemp;
-    btemp=btemp/Anorm(i);
-
-end
-%}
-
-
-%{ 
-Old version of PxDx stuff
-
-Pdxdx=zeros(size(xref));
-for i=1:Nv
-    for j=1:Nv
-        Pdxdx=Pdxdx+Qdx(:,i).*Qdx(:,j).*GHo(i,j);
-    end
-end
-
-%Calculate statistic of force Derivative using modified vector
-Pxdxdxx=zeros(size(xref));
-for i=1:Nv
-    for j=1:Nv
-        Pxdxdxx=Pxdxdxx+(Qx2(:,i).*Qdx(:,j)+Qx2(:,j).*Qdx(:,i)).*GHo(i,j);      
-    end
-end
-
-Pxx=zeros(size(xref));
-for i=1:Nv
-    for j=1:Nv
-        Pxx=Pxx+Qx2(:,i).*Qx2(:,j).*GHo(i,j);
-    end
-end
-
-%{
-for i=1:Nv
-    for j=1:Nv
-        Pxdxdxx=Pxdxdxx+(Qx2(:,i).*Qdx(:,j)+Qx2(:,j).*Qdx(:,i)).*Ginit(i,j);
-        Pxx=Pxx+Qx2(:,i).*Qx2(:,j).*Ginit(i,j);
-    end
-end
-%}
-
-
-%}
-
-%% Dirichlet Ellimination of BCS
-%{
-%BC1=Tndx(1,:);
-%BC2=Tndx(end,:);
-%BC=[BC1;BC2];
-BC=zeros(2,N);
-BC(1,1)=1;
-BC(end,end)=1;
-NullM=null(BC);
-NullM=sparse(NullM);
-Ke=NullM'*sparse(K)*NullM;
-Se=NullM'*Se*NullM;
-warning('off','MATLAB:nearlySingularMatrix')
-clear opts
-opts.issym=1;
-opts.v0=ones(size(Se,1),1);
-%opts.disp=0;
-opts.tol=eps/1e50;
-[Qe,LQ]=eigs(Ke,Se,Nv,'sm',opts);
-Q=NullM*Qe;
-%}
-%%
