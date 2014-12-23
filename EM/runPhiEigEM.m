1cb72d86d94f5aa25f55ef4da6bbafecf353f875
Kevin R. Haas <krhaas@counsyl.com>
2014-12-23 01:17:44 -0800

    cleanup the runPhiEigEM method

--- EM/runPhiEigEM.m
+++ EM/runPhiEigEM.m
@@ -1,6 +1,5 @@
 %function runPhiEigEM(varargin)
-% Get EM optimized smFRET 
-% runPhiEigEM(smFRETdata,etaF,run#)
+% Get EM optimized smFRET
 %% Run as Script
 FRETname='QuadFdatax2.mat';
 etaF=2e-7;
@@ -21,16 +20,12 @@ format long
 
 load(FRETname)
 
-
 %select trajectory number to work on.
 Fdata=Fdata(run);
 Fdata.Times=50000; %Number of photons total
 Tad=Fdata.Tad(1:Fdata.Times);
 Fdata.iTad=(Fdata.iTad(1:Fdata.Times) > eps );
 Fdata.DTs = Tad - [0; Tad(1:end-1)];
-
-
-%[xref,Tnx,Tndx,w0,Tnix,Tnddx]=getCheby(Res,N,xratio);
 
 Nh=256;%Spectral element number
 Np=8;%Spectral element order
@@ -45,7 +40,7 @@ minx=min(Fdata.xc(:,1));
 maxx=Fdata.xref(find(Fdata.rawpdf > 1e-2,1,'last'));
 minx=Fdata.xref(find(Fdata.rawpdf > 1e-2,1,'first'));
 
-
+% Set domain of solution
 xratio=(maxx-minx)/2;% Lenght L of box
 xavg=(maxx+minx)/2;% Center of Box x=r/Ro
 xratio=0.6;
@@ -80,67 +75,49 @@ dp0=-sin((xref-xavg)*pi/L)*pi/L;
 pref = exp(-fretV(xref));
 pref = pref/sum(w0.*pref);
 
+% Make global struct of integration kernels and vectors.
 Chebs{1}=x; Chebs{2}=w; Chebs{3}=Ldx; Chebs{4}=Tndx; Chebs{11}=Dnx; Chebs{12}=0; Chebs{14}=dp0;
 Chebs{5}=0; Chebs{6}=0; Chebs{7}=xref; Chebs{8}=w0; Chebs{9}=Np; Chebs{10}=Nh; Chebs{13}=p0; 
 
 
 restart=1;
 if(restart == 1 )
-    
-pCP=p0; %Cutting planes
-dpCP=dp0;
-%pCP=p0.*xref;
-%dpCP=p0+dp0.*xref;
+    pCP=p0; %Cutting planes
+    dpCP=dp0;
+    %pCP=p0.*xref;
+    %dpCP=p0+dp0.*xref;
 
-dpCP=dpCP/sqrt(sum(w0.*pCP.^2));
-pCP=pCP/sqrt(sum(w0.*pCP.^2));
+    dpCP=dpCP/sqrt(sum(w0.*pCP.^2));
+    pCP=pCP/sqrt(sum(w0.*pCP.^2));
 
 
-pB=pCP; %Optimal point
-dpB=dpCP;
-etaD=NaN;
-pyCPs=zeros(1,ceil(loops/recordIter));
-%epsilon=pyCPs;
-pyBs=pyCPs;
-%FreeEs=pyCPs;
-%ldCPs=pyCPs;
-ldBs=pyCPs;
-etaDs=pyCPs;
-etaFs=pyCPs;
-kabs=pyCPs;
-kbas=pyCPs;
-Iteration=pyCPs;
-%alphas=zeros(loops,loops);
-%gradDs=pyCPs;
-%pCPs=zeros(length(xref),loops);
-%dpCPs=pCPs;
-%ABs=pCPs;
-%Phis=pCPs;
-%dPhis=pCPs;
+    pB=pCP; %Optimal point
+    dpB=dpCP;
+    etaD=NaN;
+    pyCPs=zeros(1,ceil(loops/recordIter));
+    %epsilon=pyCPs;
+    pyBs=pyCPs;
+    %FreeEs=pyCPs;
+    %ldCPs=pyCPs;
+    ldBs=pyCPs;
+    etaDs=pyCPs;
+    etaFs=pyCPs;
+    kabs=pyCPs;
+    kbas=pyCPs;
+    Iteration=pyCPs;
 
-pBs=zeros(length(xref),ceil(loops/recordIter));
-dpBs=pBs;
-%epsilon(1)=NaN;
+    pBs=zeros(length(xref),ceil(loops/recordIter));
+    dpBs=pBs;
 
-%[pyB,gradDB,dPhi,Phi] = getPYDPYBundleEM(Fdata,pref,500,Chebs,0);
-%[pyB2,gradDB2,dPhi2,Phi2] = getPYDPYBundleEMold(Fdata,pref,500,Chebs,0);
-
-%% Optimize initial Diffusion value
-%pyNorm=getPYDPYBundleEM(Fdata,pB.^2,50,Chebs,0);
-tauI=0;
-options4 = optimset('Display','iter-detailed','GradObj','on','TolFun',1e-4,...
-        'LargeScale','off','DerivativeCheck','off','FinDiffType','central','FinDiffRelStep',1e-3);
-[ldB,pyNorm,exitflag,output,grad,hessian] = fminunc(@(ldB) getPhi(Fdata,pB.^2,ldB,Chebs,0),50,options4);
-
-%pyNorm=getPYDPYBundle2(Fdata,pB.^2,ldB,Chebs,0);
+    %% Optimize initial Diffusion value
+    options4 = optimset('Display','iter-detailed','GradObj','on','TolFun',1e-4,...
+            'LargeScale','off','DerivativeCheck','off','FinDiffType','central','FinDiffRelStep',1e-3);
+    [ldB,pyNorm,exitflag,output,grad,hessian] = fminunc(@(ldB) getPhi(Fdata,pB.^2,ldB,Chebs,0),50,options4);
 else
-    %pCP=pCPs(:,restart);
-    %dpCP=dpCPs(:,restart);
     pB=pBs(:,restart);
     dpB=dpBs(:,restart);
     etaD=etaDs(restart);
     etaF=etaFs(restart);
-    %alpha=alphas(1:restart,restart);
     ldCP=ldCPs(restart);
     ldB=ldBs(restart);
 end
@@ -151,27 +128,30 @@ for iter=restart:loops
     pB = pCP;
     dpB = dpCP;
     
-    %Goto Optimum Diffusion point
+    %Goto Optimum Diffusion point every 5 iterations.
     if( ~mod(iter,5) )
-    options4 = optimset('Display','iter-detailed','GradObj','on','TolFun',1e-4,...
-        'LargeScale','off','InitialHessType','user-supplied','InitialHessMatrix',hessian);
-    [ldB,pyB,exitflag,output,grad,hessian] = ...
-        fminunc(@(ldB) getPhi(Fdata,pB.^2,ldB,Chebs,pyNorm),ldB,options4);
-    etaD = ldB*etaF*sum(w0.*dpB.^2);
+        options4 = optimset('Display','iter-detailed','GradObj','on','TolFun',1e-4,...
+            'LargeScale','off','InitialHessType','user-supplied','InitialHessMatrix',hessian);
+        [ldB,pyB,exitflag,output,grad,hessian] = ...
+            fminunc(@(ldB) getPhi(Fdata,pB.^2,ldB,Chebs,pyNorm),ldB,options4);
+        etaD = ldB*etaF*sum(w0.*dpB.^2);
     end
-    
-    
+
+    % Get the expected Peq (E-step) and the diffusion derivative.
     [pyB,gradDB,dPhi,Phi,AB]=getPhi(Fdata,pB.^2,ldB,Chebs,pyNorm);
 
-   
+    % set the optimal peq to be equal to the observed peq (M-step)
     pstar = (pB.^2.*Phi).^(1/(1+ldB*etaF));
+    % re-normalize peq
     pstar = pstar/sum(w0.*pstar);
     pCP = sqrt(pstar);
     dpCP = Tndx*pCP;
     
-   %% Plot Iterations
-    if( ~mod(iter-1,recordIter) )
-        
+    %% Plot Iterations
+    if( mod(iter-1,recordIter) )
+        continue
+    end
+
     [kab,kba] = getMFPT(0.8,1.2,xref,-log(pB.^2),ldB);
     kabs(record)=kab;
     kbas(record)=kba;
@@ -189,8 +169,7 @@ for iter=restart:loops
     set(gcf, 'PaperUnits','inches','PaperPosition', [0.5 0.5 7.5 10]);
     subplot(3,1,1)
     
-    [AX,H1,H2] = plotyy(xref,[pB.^2 pCP.^2]...
-        ,xref,[dPhi]);
+    [AX,H1,H2] = plotyy(xref,[pB.^2 pCP.^2],xref,[dPhi]);
     title(sprintf('Diffusion = %0.4G', ldB))
     set(AX(1),'XLim',[xref(1) xref(end)]);
     set(AX(1),'YLim',[0 10]);
@@ -210,8 +189,7 @@ for iter=restart:loops
    	
     
     %legend('Log-Likelihood','\epsilon = J^*-J_C_P','Location','North','Orientation','horizontal')
-    
-    
+
     subplot(3,1,3)
     [AX] = plot(Iteration(1:record), (ldBs(1:record)),'-o');
     xlabel('Iteration #')
@@ -224,11 +202,8 @@ for iter=restart:loops
     
     save([pwd '/EMdata/' FRETname sprintf('etaF%.2dEM%.2d.mat',etaF,iter)],...
         'ldB','pB','dpB','Phi','dPhi','xref','etaD','kab','kba') 
-    
-    
+
     record=record+1;
-    end
-   
    
 end
 
